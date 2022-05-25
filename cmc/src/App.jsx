f4cb992eec85116e0806895327031bcb3272a1c8
Hanzothe <gui_oliveirasantos123@hotmail.com>
2022-05-25 12:02:46 -0300

    master

---
+++ cmc/src/App.jsx
@@ -0,0 +1,14 @@
+import './global.css'
+
+function App() {
+  return (
+    
+    <div className='App h-screen bg-black flex justify-center items-center'>
+<input type='text' className='border-solid border-zinc-500 border-2 h-20 w-96' />
+<button className='border-solid border-2 bg-zinc-500 border-white text-white h-20 w-60'>Convert</button>
+    </ div>
+
+)
+}
+
+export default App
